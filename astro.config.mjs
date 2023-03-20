92332df6813b3d2041663ea479f9779d1f11df8d
t3chcrazy <abhishekprashant09@gmail.com>
2023-03-21 00:30:44 +0530

    Config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://t3chcrazy.github.io",
-    base: "/"
+    output: "static",
+    build: {
+        assets: "/"
+    }
 });
