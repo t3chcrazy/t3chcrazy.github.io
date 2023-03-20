37c81f0b9b6cd6126b39c99ec1a8ffe6f8beb966
t3chcrazy <abhishekprashant09@gmail.com>
2023-03-21 00:22:37 +0530

    Small config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://t3chcrazy.github.io"
+    site: "https://t3chcrazy.github.io",
+    base: "/"
 });
