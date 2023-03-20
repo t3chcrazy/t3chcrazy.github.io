4c28defe7bc93f718cc12c31c27eec3f03b688e9
t3chcrazy <abhishekprashant09@gmail.com>
2023-03-21 02:19:09 +0530

    Meta tags added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://t3chcrazy.github.io",
-    base: "/Portfoliov2"
+    site: "https://t3chcrazy.github.io"
 });
