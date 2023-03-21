faeb280891debaaf406c6e193baef7f4d4c82820
t3chcrazy <abhishekprashant09@gmail.com>
2023-03-21 23:58:09 +0530

    Image optimizations, some content changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://t3chcrazy.github.io"
+  site: "https://t3chcrazy.github.io",
+  integrations: [image()]
 });
