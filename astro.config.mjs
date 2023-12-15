5cff8b1ab36f36db0e31a332a15abd991b2b2850
t3chcrazy <abhishekprashant09@gmail.com>
2023-12-16 03:12:36 +0530

    Upgraded astro to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://t3chcrazy.github.io",
-  integrations: [image()]
 });
