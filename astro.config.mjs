3ee5180ab3b6f46035b07f44cb93c0df7a2713ad
t3chcrazy <abhishekprashant09@gmail.com>
2025-07-15 01:59:04 +0530

    feat: Add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://t3chcrazy.github.io",
+  integrations: [sitemap({
+    customPages: [
+      "./resume.pdf"
+    ]
+  })],
 });
